cc46947cd8d01a5f06202293d8d62dece36492dc
Svetlana <vashenglish@gmail.com>
2026-03-23 05:20:59 +0300

    Initial React/Vite setup for travel-site

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,4 @@
-import { defineConfig } from 'vite';
-
-// Конфиг Vite для GitHub Pages
 export default defineConfig({
 // root можно не указывать, по умолчанию '.'
-base: '/travel site/', // ВАЖНО: имя репозитория c учётом регистра
+base: '/travel-site/', // ВАЖНО: имя репозитория c учётом регистра
 });
